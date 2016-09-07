222dacd32b81f1ad2193bdfa82446b566fa9290b
chao-ouyang <chao-ouyang@users.noreply.github.com>
2016-09-06 17:15:48 -0700

    cherry-pick hotfix sprint1-task#002

--- CodeFile.cs
+++ CodeFile.cs
@@ -1,4 +1,4 @@
 // sprint 1, Task# 001
-// sprint 1, Task# 002, in feature brnach, more coding...
+// sprint 1, Task# 002, in feature brnach, more coding...; hotfix!
 // sprint 2, Task# 004,
 // sprint 2, Task# 003,
