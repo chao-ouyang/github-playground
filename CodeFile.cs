26542d7ada945c53df41fbf63b4a3dcd84cf61c7
chao_ouyang <chao.ouyang@bydeluxe.com>
2017-02-14 16:42:11 -0800

    task2 chao_ouyang

--- CodeFile.cs
+++ CodeFile.cs
@@ -1,8 +1,1 @@
-test demo line 1
-//chao working on issue002
-//chao working more
-<<<<<<< HEAD
-//Sumi Changes
-Release branch line changed 
-//New changes for task 2 
->>>>>>> develop
+task2 - chao_ouyang
